f2acfd5ac9d8bc39d6eafd59ca10f4597b02d916
Nb Kevin <k.memo@live.cn>
2015-01-12 20:29:28 +0800

    add: get typo suggestion add: console refresh, platform related fix: several bugs

--- Dict.py
+++ Dict.py
@@ -4,7 +4,8 @@
 __author__ = 'Nb'
 
 import requests
-import sys
+import os, sys
+import platform
 
 
 class Dictionary():
@@ -18,18 +19,52 @@ class Dictionary():
     }
 
     def __init__(self, argv):
-        if len(argv) == 1:
+        if len(argv) == 0:
+            self.queryAddress = self.queryAddress + input('enter the word you want to query: ')
+            self.translate()
+        elif len(argv) == 1:
             self.queryAddress = self.queryAddress + argv[0]
             self.translate()
         else:
             print('Input argv error: can only accept one word')
 
     def translate(self):
+        print('\n            Waiting for response...')
         response = requests.get(self.queryAddress, headers=self.headerDict).text
+
+        #refresh console
+        if platform.system().lower() == 'windows':
+            os.system('cls')
+        elif platform.system().lower() == 'linux':
+            os.system('clear')
+
+        #check response
+        #no entry found
         if response.count('error-wrapper') > 0 and response.count('error-typo') == 0 :
-            print('抱歉，查無此詞')
+            print('\n          抱歉，查無此詞')
+
+        #no entry found but with suggestions
+        elif response.count('error-wrapper') > 0 and response.count('error-typo') > 0 :
+            #find suggestion container
+            suggesContainer = response.split('<div class="error-typo">')[1].split('</div>')[0]
+
+            #find all sugggestions
+            suggesCount = suggesContainer.count('</a>')
+            suggesList = []
+            for i in range (1, suggesCount+1):
+                suggestion = suggesContainer.split('</a>')[i-1].split('<a')[1].split('>')[1]
+                suggesList.append(suggestion)
+
+            #print results
+            print('\n          抱歉，查無此詞')
+            print('          您是不是想要查 %s' % suggesList[0])
+            if len(suggesList) > 1:
+                for i in range(1, len(suggesList)):
+                    print('                         %s' % suggesList[i])
+
+        #entry found
         else:
-            #find result section
+            #find result container
             transContainer = response.split('<div class="trans-container">')[1].split('</div>')[0]
 
             #find all results
@@ -40,10 +75,11 @@ class Dictionary():
                 transList.append(explanation)
 
             #print results
-            print('      Input: %s' % self.queryAddress.split('q=')[1])
+            print('\n      Input: %s' % self.queryAddress.split('q=')[1])
             print('Explanation: %s' % transList[0])
-            for i in range(1, len(transList)):
-                print('             %s' % transList[i])
+            if len(transList) > 1:
+                for i in range(1, len(transList)):
+                    print('             %s' % transList[i])
 
 if __name__ == '__main__':
     Dictionary(sys.argv[1:])
